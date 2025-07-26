86f606c9be31086eb3d1dafe88ed17f000e12464
Levin Paul <levinpaul197@gmail.com>
2025-07-26 06:27:46 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,14 +1,13 @@
-
 import { initializeApp, getApps, getApp, FirebaseOptions } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "",
+  apiKey: "AIzaSyCTjAWqGUnxDV4YeUxYHbogfRvs5cZTTIg",
+  authDomain: "chargeone.firebaseapp.com",
+  projectId: "chargeone",
+  storageBucket: "chargeone.firebasestorage.app",
+  messagingSenderId: "669583586032",
+  appId: "1:669583586032:web:c0c73391c7eb691c7ec835"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
