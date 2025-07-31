787ecb60060cbc6fedfd49d661958ad565e78b33
Levin Paul <levinpaul197@gmail.com>
2025-07-31 20:36:29 +0000

    now fix up the route planner logic , the current location isnt updated p

--- src/components/charge-one/RoutePlanner.tsx
+++ src/components/charge-one/RoutePlanner.tsx
@@ -18,6 +18,17 @@ export default function RoutePlanner({ onPlanRoute, isPlanning, currentLocation 
   const [origin, setOrigin] = useState('');
   const [destination, setDestination] = useState('');
 
+  useEffect(() => {
+    // Automatically set origin when current location is first determined,
+    // but only if the user hasn't already typed something in.
+    if (currentLocation && origin === '') {
+      setOrigin(`${currentLocation.lat}, ${currentLocation.lng}`);
+    }
+    // We only want this to run when the location is first found, or if origin is cleared.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentLocation]);
+
+
   const handlePlanRoute = () => {
     if (origin && destination) {
       onPlanRoute(origin, destination);
